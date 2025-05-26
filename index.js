30eef80e65a54298f46e90944ef02624857ea94b
ihamedr <amirhamedrazavi@gmail.com>
2025-05-26 20:57:00 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.get('/proxy', async (req, res) => {
   const { action, userId, word } = req.query;
 
-  const scriptUrl = `https://script.google.com/macros/s/AKfycbyRG94adV_4DXmb9YKUqHw81fI1HpptRvPfIiwn5FoNu5iBQe_scQKxzd5zTL7mZVXH5A/exec?action=${action}&userId=${userId}${word ? &word=${encodeURIComponent(word)} : ''}`;
+  const scriptUrl = `https://script.google.com/macros/s/AKfycbyRG94adV_4DXmb9YKUqHw81fI1HpptRvPfIiwn5FoNu5iBQe_scQKxzd5zTL7mZVXH5A/exec?action=${action}&userId=${userId}${word ? `&word=${encodeURIComponent(word)}` : ''}`;
 
   try {
     const response = await fetch(scriptUrl);
